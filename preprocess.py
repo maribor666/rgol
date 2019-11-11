25b4497efde3e5d3df87939384c5c63bf97e9cb6
maribor666 <spekaoleh@ukr.net>
2019-11-12 02:25:36 +0300

    preprocess mostly done

---
+++ preprocess.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas
+
+dataset_path = "resources/train.csv"
+
+
+def main():
+    # it will be in main
+    df = pandas.read_csv(dataset_path)
+    X = df.iloc[:, 402:].values
+    Y = df.iloc[:, 2:402].values
+    # end of main code
+    prepareX(X)
+    # Y_train = Y.reshape(-1, 1).ravel()
+
+    # print(Y_train.shape)
+    print(Y.shape)
+
+
+def prepareY(Y):
+    return Y
+
+
+def prepareX(X, wind_size=3):
+    X = X.reshape((-1, 20, 20))
+    X_train = []
+    length = len(X)
+    for i in range(length):
+        game = X[i]
+        game = np.c_[game, np.full((game.shape[0]), -1)]
+        game = np.c_[np.full((game.shape[0]), -1), game]
+        game = np.r_[game, [np.full((game.shape[1]), -1)]]
+        game = np.r_[[np.full((game.shape[1]), -1)], game]
+        windows = rolling_window(game)
+        windows = windows.reshape((400, wind_size, wind_size))
+        X_train.append(windows)
+    print(len(X_train))
+    print(X_train[4].shape)
+    return X_train
+
+
+def rolling_window(a, shape=(3, 3)):
+    # rolling window for 2D array
+    #   https://stackoverflow.com/questions/15722324/sliding-window-of-m-by-n-shape-numpy-ndarray
+    s = (a.shape[0] - shape[0] + 1,) + (a.shape[1] - shape[1] + 1,) + shape
+    strides = a.strides + a.strides
+    return np.lib.stride_tricks.as_strided(a, shape=s, strides=strides)
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -20,37 +20,19 @@ def main():
 	df = pandas.read_csv(dataset_path)
 	X = df.iloc[:, 402:].values
 	Y = df.iloc[:, 2:402].values
+	
 	X_train = X.reshape((-1, 20, 20))
 	a = X_train[3]
+	
+	
+	
 	test = rolling_window(a, (3, 3))
 	print(a)
-	# print(test)
 	print(test.shape)
 	print(test[0][0])
-
-	# a = np.array([[0,  1,  2,  3,  4,  5],
- #                  [6,  7,  8,  9, 10,  11],
- #                  [12, 13, 14, 15, 7,   8],
- #                  [18, 19, 20, 21, 13, 14],
- #                  [24, 25, 26, 27, 19, 20],
- #                  [30, 31, 32, 33, 34, 35]], dtype=np.int)
-	# b = np.arange(36, dtype=np.float).reshape(6,6)
-	# present = np.array([[7,8],[13,14],[19,20]], dtype=np.int)
-	# absent  = np.array([[7,8],[42,14],[19,20]], dtype=np.int)
-
-	# print(rolling_window(a, present.shape))
-	# found = np.all(np.all(rolling_window(a, present.shape) == present, axis=2), axis=2)
-	# print(np.transpose(found.nonzero()))
-	# found = np.all(np.all(rolling_window(b, present.shape) == present, axis=2), axis=2)
-	# print(np.transpose(found.nonzero()))
-	# found = np.all(np.all(rolling_window(a, absent.shape) == absent, axis=2), axis=2)
-	# print(np.transpose(found.nonzero()))
 	
 
-def rolling_window(a, shape):  # rolling window for 2D array
-    s = (a.shape[0] - shape[0] + 1,) + (a.shape[1] - shape[1] + 1,) + shape
-    strides = a.strides + a.strides
-    return np.lib.stride_tricks.as_strided(a, shape=s, strides=strides)
+
 
 
 
