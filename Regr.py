ae6c3ab76099326ab9ff4ec57f7b36d88192dac8
maribor666 <spekaoleh@ukr.net>
2019-11-28 20:04:05 +0200

    add: loss calculating paralleling and cython fast functions. theta updating is too slow

--- Regr.py
+++ Regr.py
@@ -1,10 +1,16 @@
+import multiprocessing as mp
 import numpy as np
 import pandas
-import cython
+# import cython
 
 from preprocess import prepareX, prepareY
 
+import pyximport
+pyximport.install(language_level=3)
+from fastFuncs import *
+
 # mb calc new thetas after ever x_ij, y_ij
+# !!!! add 1 to left of raveled X[i][i] !!!! if needed
 
 class Regression(object):
 
@@ -12,42 +18,27 @@ class Regression(object):
         self.theta = []
         self.nums = 0
 
-    def _hypothesis(self, X):
-        spliced = X.ravel()
-        x = np.insert(spliced, 0, 1)
-        matr_mult = ((-1) * self.theta).T * x
-        return 1 / (1 + np.exp(matr_mult.sum()))
-        
-    def _loss(self, X, y):
-        # cdef double summa = 0
-        summa = 0
-        i = 0
-        for x_i, y_i in zip(X, y):
-            for x_ij, y_ij in zip(x_i, y_i):
-                summa += self._cost(self._hypothesis(x_ij), y_ij)
-                i += 1
-                print(i)
-                break
-            break
-        return summa / self.nums
-            
-
-    def _cost(self, hyp, y):
-        if y == 1:
-            return (-1) * np.log(hyp)
-        else:
-            return (-1) * np.log(1 - hyp)
-
     def train(self, X, y, epoches=100):
-        # print(X[3][3])
-        # print(y[3][3])
-        self.theta = np.random.randn(10)
+        self.cpu_count = mp.cpu_count()
+        self.theta = np.random.randn(9)
         self.nums = len(X) * X[0].shape[0]
+        print(f"cpu count {self.cpu_count}")
+        print(len(X))
+        print(X[0].shape)
+        splitedX = np.array_split(X, self.cpu_count)
+        splitedY = np.array_split(y, self.cpu_count)
+        print("shape of splited part",splitedY[0].shape)
+        # print(sum(len(el) for el in splited))
+        self.pool = mp.Pool(self.cpu_count)
+        input_data = [(x_part, y_part, self.theta) for x_part, y_part in zip(splitedX, splitedY)]
+        res = self.pool.starmap(loss, input_data)
+        summed_res = sum(res) / self.nums
+        print(summed_res)
         # print(self.theta)
-        h = self._hypothesis(X[3][3])
-        # print(h)
-        loss = self._loss(X, y)
-        print(loss)
+        # self.theta = update_theta(X, y, self.theta, self.nums)
+        print(self.theta)
+
+
 
     def predict(self, X):
         pass
@@ -65,3 +56,42 @@ if __name__ == '__main__':
 
     regr = Regression()
     regr.train(X_train, Y_train)
+    print("end")
+
+
+    # def _hypothesis(self, X):
+    #     spliced = X.ravel()
+    #     x = np.insert(spliced, 0, 1)
+    #     matr_mult = ((-1) * self.theta).T * x
+    #     return 1 / (1 + np.exp(matr_mult.sum()))
+        
+    # def _loss(self, X, y):
+    #     # cdef double summa = 0
+    #     summa = 0
+    #     i = 0
+    #     for x_i, y_i in zip(X, y):
+    #         for x_ij, y_ij in zip(x_i, y_i):
+    #             summa += self._cost(self._hypothesis(x_ij), y_ij)
+    #             i += 1
+    #             print(i)
+    #             break
+    #         break
+        # return summa / self.nums
+            
+
+    # def _cost(self, hyp, y):
+    #     if y == 1:
+    #         return (-1) * np.log(hyp)
+    #     else:
+    #         return (-1) * np.log(1 - hyp)
+
+    # def train(self, X, y, epoches=100):
+        # print(X[3][3])
+        # print(y[3][3])
+        # self.theta = np.random.randn(10)
+        # self.nums = len(X) * X[0].shape[0]
+        # print(self.theta)
+        # h = self._hypothesis(X[3][3])
+        # print(h)
+        # loss = self._loss(X, y)/
+        # print(loss)
---
+++ fastFuncs.pyx
@@ -0,0 +1,50 @@
+cimport cython
+
+import numpy as np
+
+from libc.math cimport log, exp
+cimport numpy as np
+
+@cython.cdivision(True)
+cpdef double hypothesis(theta, x): # add type hints for arguments
+	# print(theta.shape)
+	# print(x.shape)
+	cdef double matr_mul = np.dot(theta, x)
+	return 1 / (1 + exp(matr_mul))
+
+cpdef double loss(X, Y, theta): # 'np' is not a cimported module. need cimport to add type hints
+	print(type(X), X.shape)
+	print(type(Y), Y.shape)
+	x_test = np.ravel(X[0][0])
+	y_test = Y[0][0]
+	print(x_test, y_test)
+	print(type(theta))
+	cdef double res = 0
+	cdef double h = 0
+	# res = hypothesis(theta, x_test)
+	for xi, yi in zip(X, Y):
+		for xij, yij in zip(xi, yi):
+			h = hypothesis(theta, np.ravel(xij))
+			res += cost(h, yij)
+	return res
+
+cpdef double cost(h, y): # add type hints for arguments
+	if y == 1:
+		return -log(h)
+	else:
+		return -log(1 - h)
+
+cpdef update_theta(X, Y, old_theta, nums, alfa=0.05):
+	# cdef np.array[double, dim=1] new_theta = old_theta
+	cdef double res = 0
+	cdef double summa = 0
+	cdef double[:] new_theta = np.zeros((9,))
+	for theta_j in range(len(old_theta)):
+		for xi, yi in zip(X, Y):
+			for xij, yij in zip(xi, yi):
+				xij_raveled = np.ravel(xij)
+				h = hypothesis(old_theta, xij_raveled)
+				res = (h - yij) * xij_raveled[theta_j]
+				summa += res
+		new_theta[theta_j] = old_theta[theta_j] - (alfa / nums) * summa
+	return new_theta
