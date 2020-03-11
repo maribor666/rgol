f82e317c88edf8d38cdefaeb5ee19bf52d13bb41
maribor666 <spekaoleh@ukr.net>
2020-03-11 19:57:13 +0200

    add: random choising for RF

--- RF.py
+++ RF.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 import pandas
 import preprocess
+import dt_my
 
 dataset_path = "resources/train.csv"
 
@@ -16,6 +17,21 @@ def main():
     print(X_train.shape)
     print(Y_train.shape)
 
+    samples_num = np.random.choice(Y_train.shape[0], Y_train.shape[0] * 2 // 3, replace=True)
+    x_subset = X_train[samples_num, :]
+    y_subset = Y_train[samples_num]
+    print(samples_num[:20])
+
+    tree = dt_my.build_tree(x_subset, y_subset)
+
+
+    # t = np.arange(5, 10)
+    # print(t)
+    # samples_num = np.random.choice(t.shape[0], 3)
+    # print(samples_num)
+    # print(t[samples_num])
+
+    # A[np.random.choice(A.shape[0], 2, replace=False), :]
 
 
 
